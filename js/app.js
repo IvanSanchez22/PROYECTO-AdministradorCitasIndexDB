bc6d2c770173582cf5d2708f8dee0dd99d2f9f21
Iván Sánchez <ivansanchezlabrador22@gmail.com>
2022-09-30 17:41:21 +0200

    Creando la base de datos

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+let DB;
 
 const mascotaInput = document.querySelector('#mascota');
 const propietarioInput = document.querySelector('#propietario');
@@ -18,6 +19,14 @@ const heading = document.querySelector('#administra');
 
 
 let editando = false;
+
+
+window.onload = () => {
+    eventListeners();
+
+    crearDB();
+
+}
 
 
 // Eventos
@@ -261,5 +270,44 @@ function cargarEdicion(cita) {
     formulario.querySelector('button[type="submit"]').textContent = 'Guardar Cambios';
 
     editando = true;
+
+}
+
+function crearDB(){
+    //Crear la base de datos en versión 1.0
+    const crearDB = window.indexedDB.open('citas', 1);
+
+    //Si hay un error
+    crearDB.onerror = function(){
+        console.log('Hubo un error');
+    }
+
+    //Si todo va bien
+    crearDB.onsuccess = function(){
+        console.log('BD creada');
+        DB = crearDB.result;
+        console.log(DB);
+    }
+
+    //Definir el schema
+    crearDB.onupgradeneeded = function(e) {
+        const db = e.target.result;
+
+        const objectStore = db.createObjectStore('citas', {
+            KeyPath: 'id',
+            autoIncrement: true
+        });
+
+        //Definir las columnas
+        objectStore.createIndex('mascota', 'mascota', { unique: false });
+        objectStore.createIndex('propietario', 'propietario', { unique: false });
+        objectStore.createIndex('teléfono', 'teléfono', { unique: false });
+        objectStore.createIndex('fecha', 'fecha', { unique: false });
+        objectStore.createIndex('hora', 'hora', { unique: false });
+        objectStore.createIndex('síntomas', 'síntomas', { unique: false });
+        objectStore.createIndex('id', 'id', { unique: true });
+
+        console.log('DB creada y lista');
+    }
 
 }
